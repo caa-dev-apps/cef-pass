2110b1fe30f5eabef7b26bd2780ecb9e3f21142f
spw <spw.carnakib@gmail.com>
2015-06-27 22:21:50 +0100

    cleaning up Integration tests with Spock parameters + @Unroll

--- app/src/test/groovy/cefpass/I_RS2_MetaObjects_Tests.groovy
+++ app/src/test/groovy/cefpass/I_RS2_MetaObjects_Tests.groovy
@@ -5,9 +5,11 @@ import org.springframework.util.ResourceUtils
 import cefpass.App
 import cefpass.CefParser
 import exceptions.RS2_MetaObjectsException
+import spock.lang.Unroll
 
 import Helper_Resources;
 
+//x @Unroll
 class I_RS2_MetaObjects_Tests extends Specification{
 
     def setup()
@@ -61,7 +63,7 @@ class I_RS2_MetaObjects_Tests extends Specification{
     // done
     def "R_2_04___FILE_TYPE_MUST_BE_CEF"() 
     {
-        given:
+        given:      
             def App app = new App()
         when:
             def result = app.stages(l_cmd_args)
@@ -137,7 +139,7 @@ class I_RS2_MetaObjects_Tests extends Specification{
             l_cmd_args << Helper_Resources.getCommandLineArgsList('R_2_09___FILE_TIME_SPAN_MUST_BE_ISO_TIME_RANGE')
             
     }
-
+    
     def "R_2_10___FILE_TIME_SPAN_START_TIME_MUST_BE_BEFORE_STOP_TIME"() 
     {
         given:
@@ -149,8 +151,8 @@ class I_RS2_MetaObjects_Tests extends Specification{
             ex.getError() == RS2_MetaObjectsException.Error.R_2_10___FILE_TIME_SPAN_START_TIME_MUST_BE_BEFORE_STOP_TIME
         where :
             l_cmd_args << Helper_Resources.getCommandLineArgsList('R_2_10___FILE_TIME_SPAN_START_TIME_MUST_BE_BEFORE_STOP_TIME')
-            
     }
+
     
 //todo      def "R_2_11___DATA_RECORD_TIME_STAMP_MUST_BE_BETWEEN_FILE_TIME_SPAN"() 
 //todo      {
@@ -165,6 +167,62 @@ class I_RS2_MetaObjects_Tests extends Specification{
 //todo          
 //todo      }
 
+
+//x     def getEntryTestValues(String[])
+//x     {
+//x             [
+//x             [   test_name: "First test"    ,test_num: 1        ,a: 0 ,b: 1 ], 
+//x             [   test_name: "Second test"   ,test_num: 2        ,a: 1 ,b: 2 ], 
+//x             [   test_name: "Third test"    ,test_num: 3        ,a: 2 ,b: 3 ], 
+//x             [   test_name: "Fourth test"   ,test_num: 4        ,a: 3 ,b: 4 ], 
+//x             [   test_name: "Fifth test"    ,test_num: 5        ,a: 4 ,b: 5 ], 
+//x             ]
+//x     }
+
+    @Unroll
+    def "R_2_10___XXXX #test_name #test_num"() 
+    {
+        given:
+            def App app = new App()
+        when:
+            a = a + 1
+        then:
+            a == b
+        where :
+            test_name       | test_num | a | b
+            "First test"    | 1        | 0 | 1
+            "Second test"   | 2        | 1 | 2
+            "Third test"    | 3        | 2 | 3
+            "Fourth test"   | 4        | 3 | 4
+            "Fifth test"    | 5        | 4 | 5
+    }
+    
+
+//x     def getEntryTestValues(String[] i_imputs)
+    def getEntryTestValues(i_vals)
+    {
+           [
+            [    "First test"    , 1        , 0 , 1 ], 
+            [    "Second test"   , 2        , 1 , 2 ], 
+            [    "Third test"    , 3        , 2 , 3 ], 
+            [    "Fourth test"   , 4        , 3 , 4 ], 
+            [    "Fifth test"    , 5        , 4 , 5 ], 
+            ]            
+    }
+
+    @Unroll
+    def "R_2_10___YYYY #test_name #test_num"() 
+    {
+        given:
+            def App app = new App()
+        when:
+            a = a + 1
+        then:
+            a == b
+        where:
+            [test_name, test_num, a, b] <<  getEntryTestValues([1,2,3,4])
+            
+    }
 }
 
     
